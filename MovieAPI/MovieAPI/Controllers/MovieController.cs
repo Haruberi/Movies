f4a3ee5289bfb51c8595631ca9e14aa8668380c4
Anna <anna.h.hallberg.92@gmail.com>
2020-12-28 13:20:26 +0100

    Add model and properties

--- MovieAPI/MovieAPI/Controllers/MovieController.cs
+++ MovieAPI/MovieAPI/Controllers/MovieController.cs
@@ -1,16 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MovieAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class MovieController : Controller
     {
+        //cache
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var url = "https://ghibliapi.herokuapp.com";
+
+            string serializedResponse;
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    var responseCont = response.Content;
+                    var stringResp = await responseCont.ReadAsStringAsync();
+                    serializedResponse = JsonConvert.SerializeObject(stringResp);
+                }
+            }
+
+            return Ok(serializedResponse);
         }
     }
 }
